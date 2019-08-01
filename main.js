b63d4ace39a5ae9d5cd59494d343323909137224
Jean Honlet <jehon@users.noreply.github.com>
2019-08-01 14:20:36 +0200

    Add main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node --experimental-modules
+
+import start from './server/server.mjs';
+import open from 'open';
+import childProcess from 'child_process';
+
+
+start().then(() => {
+	console.log('Launching browser');
+	const browserApp = childProcess.exec('chromium-browser http://localhost:3000');
+	browserApp.stdout.pipe(process.stdout);
+	browserApp.stderr.pipe(process.stderr);
+	browserApp.on('exit', () => {
+		console.info('end of browser');
+		process.exit(0);
+	});
+});
