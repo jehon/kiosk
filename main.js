f68549894e8cb379de600f661ee7cc715d1aac36
Jean Honlet <jehon@users.noreply.github.com>
2019-08-02 09:22:16 +0200

    Make starting inside the main working

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node --experimental-modules
 
 import start from './server/server.mjs';
-import childProcess from 'child_process';
 
-start().then(() => {
-	console.log('Launching browser');
-	const browserApp = childProcess.exec('chromium-browser http://localhost:3000');
-	browserApp.stdout.pipe(process.stdout);
-	browserApp.stderr.pipe(process.stderr);
-	browserApp.on('exit', () => {
-		console.info('end of browser');
-		process.exit(0);
-	});
+import * as browser from './server/browser.js';
+
+start().then((port) => {
+	browser.start(port);
 });
