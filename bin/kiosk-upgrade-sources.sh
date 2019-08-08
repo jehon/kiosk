72eb5a802af841c7e3c54c87f7594f8f35372e3e
Jean Honlet <jehon@users.noreply.github.com>
2019-08-08 21:16:32 +0200

    Make the dm restart even if package.json is up-to-date

--- bin/kiosk-upgrade-sources.sh
+++ bin/kiosk-upgrade-sources.sh
@@ -19,22 +19,22 @@ pushd "$KIOSK_APP" > /dev/null
 
 if [ -r package-lock.json ] && [ package-lock.json -nt package.json ]; then
 	header "Already up-to-date"
-	exit 0
+else
+
+	header "Need an update"
+	header_sub "** install **"
+	# See https://docs.npmjs.com/misc/scripts
+	npm install --unsafe-perm
+
+	header_sub "** prune **"
+	npm prune --unsafe-perm || true
+
+	header "** done **"
+	touch package-lock.json
+
+	header "** apply patches **"
+	"$KIOSK_APP"/apply-patches.sh
 fi
-
-header "Need an update"
-header_sub "** install **"
-# See https://docs.npmjs.com/misc/scripts
-npm install --unsafe-perm
-
-header_sub "** prune **"
-npm prune --unsafe-perm || true
-
-header "** done **"
-touch package-lock.json
-
-header "** apply patches **"
-"$KIOSK_APP"/apply-patches.sh
 
 header "Restarting the service"
 "$KIOSK_APP"/bin/kiosk-restart.sh
