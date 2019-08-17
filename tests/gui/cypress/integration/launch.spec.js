d8a1428a3b6349ce233e931ed07532c50f226c5a
Jean Honlet <jehon@users.noreply.github.com>
2019-08-17 20:50:54 +0200

    Push menu into menu-client.js

--- tests/gui/cypress/integration/launch.spec.js
+++ tests/gui/cypress/integration/launch.spec.js
@@ -1,23 +1,22 @@
 
-describe('The application is launching', function() {
-	it('Visits the page and interact with menu', function() {
+describe('The application is launching', () => {
+	it('Visits the page and interact with menu', () => {
 		cy.visit('/');
 
 		// Check existence of top elements
 		cy.get('#app-menu').should('be.visible');
 		cy.get('#main-application').should('be.visible');
 
-		// It is loaded
+		// Go to the menu
 		cy.get('#app-menu').click();
+		cy.get('kiosk-menu').should('be.visible');
 
-		// cy.get('#main-menu > *:nth-child(3)').should('be.visible');
+		// Go to clock
+		cy.get('[data-app=clock]').click();
+		cy.get('kiosk-clock').should('be.exist');
 
-		// // // Go to clock (2d element)
-		// cy.get('#main-menu > *:nth-child(2)').click();
-		// cy.get('#main-application kiosk-clock');
-
-		// // Go to links (3d element)
-		// cy.get('#main-menu > *:nth-child(3)').click();
-		// cy.get('#main-application kiosk-menu');
+		// Go to the menu
+		cy.get('#app-menu').click();
+		cy.get('kiosk-menu').should('be.visible');
 	});
 });
