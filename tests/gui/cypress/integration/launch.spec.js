477cf647c8d5d8fde0ee218676c04312fef8c173
Jean Honlet <jehon@users.noreply.github.com>
2019-08-27 18:00:00 +0200

    Take screenshots

--- tests/gui/cypress/integration/launch.spec.js
+++ tests/gui/cypress/integration/launch.spec.js
@@ -4,18 +4,23 @@ describe('The application is launching', () => {
 		cy.visit('/');
 
 		// Check existence of top elements
-		cy.get('#main-application').should('be.visible');
+		cy.get('#main-application').should('be.visible')
+			.screenshot();
 
 		// Check the existence of the menu
-		cy.get('body').trigger('mousemove', { which: 1, clientX:   1, clientY: 1 });
-		cy.wait(100);
-		cy.get('body').trigger('mousemove', { which: 1, clientX: 600, clientY: 600 });
+		cy.get('body')
+			.trigger('mousemove', { which: 1, clientX:   1, clientY: 1 })
+			.wait(100)
+			.get('body').trigger('mousemove', { which: 1, clientX: 600, clientY: 600 })
+			.screenshot();
 
 		cy.get('#app-menu').should('be.visible');
 
 		// Go to the menu
 		cy.get('#app-menu').click();
-		cy.get('kiosk-menu').should('be.visible');
+		cy.get('kiosk-menu')
+			.should('be.visible')
+			.screenshot();
 
 		// Go to clock
 		cy.get('[data-app=clock]').click();
