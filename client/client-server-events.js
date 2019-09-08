86c2e096d95a456ff3a913f058f01f8b244a4da4
Jean Honlet <jehon@users.noreply.github.com>
2019-09-08 17:25:22 +0200

    Refactor client imports

---
+++ client/client-server-events.js
@@ -0,0 +1,30 @@
+
+import ClientAPIFactory from './client-api.js';
+
+const app = ClientAPIFactory('core-api-evnets');
+const logger = app.logger;
+
+//
+// Listen from server events
+//
+
+// https://www.npmjs.com/package/sse-pusher
+var osEvents = new EventSource('/core/events');
+
+osEvents.onmessage = function (event) {
+	const data = JSON.parse(event.data, JSONDateParser);
+	logger.debug(`from server ${data.type}: ${JSON.stringify(data.data)}`);
+	app.dispatch(data.type, data.data);
+};
+
+// Use by osEvents.onmessage here, available for external use
+export function JSONDateParser(key, value) {
+	const reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+
+	if (typeof value === 'string') {
+		if (reISO.exec(value)) {
+			return new Date(value);
+		}
+	}
+	return value;
+}
