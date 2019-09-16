5772e38ee866821b650f3e4b78bc2cedb3f769a4
Jean Honlet <jehon@users.noreply.github.com>
2019-09-16 14:50:59 +0200

    Add logs

--- client/client-helpers.js
+++ client/client-helpers.js
@@ -1,14 +1,22 @@
 
+import loggerFactory from './client-api-logger.js';
+const logger = loggerFactory('core:client:helpers');
+
 // clock-client
 export async function onDate(date) {
+	logger.debug('onDate: Programming for ', date);
 	return new Promise((resolve, _reject) => {
 		if (typeof(onDate) == 'string') {
 			date = new Date(date);
 		}
 		const now = new Date();
 		if (date < now) {
+			logger.debug('onDate: but it was already in the past, triggering immediately');
 			return resolve();
 		}
-		setTimeout(() => resolve(), date - now);
+		setTimeout(() => {
+			logger.debug('onDate: is now the wanted time ', date);
+			resolve();
+		}, date - now);
 	});
 }
