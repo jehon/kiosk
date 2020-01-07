01fb303adc6a629e8b43c9dab72a615aed018df7
Jean Honlet <jehon@users.noreply.github.com>
2020-01-07 18:00:00 +0100

    wip: window show up, but throught http

--- main.js
+++ main.js
@@ -7,11 +7,16 @@ const browser = require('./server/core-browser.js');
 const loggerFactory = require('./server/server-logger.js');
 const logger = loggerFactory('core:main');
 
-startServer().then((port) => {
-	logger.info(`Server up and running on port ${port}`);
-	if (!getConfig('core.serverOnly', false)) {
+const { app: electronApp } = require('electron');
+const port = getConfig('core.port');
+
+electronApp.on('ready', () => {
+	logger.debug('Elector: on ready fired');
+	startServer(port).then((port) => {
+		logger.info(`Server up and running on port ${port}`);
 		browser.start(port);
-	} else {
-		logger.info('Not launching the browser as requested');
-	}
+	});
 });
+
+// Quit when all windows are closed.
+electronApp.on('window-all-closed', () => electronApp.quit());
