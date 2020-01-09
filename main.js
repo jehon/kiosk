54fe8e312674bd34b0d78a2de076924c422514b5
Jean Honlet <jehon@users.noreply.github.com>
2020-01-09 21:17:47 +0100

    wip: security

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ app.on('ready', () => {
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => app.quit());
+
+// https://electronjs.org/docs/tutorial/security
+app.on('web-contents-created', (event, contents) => {
+	// https://electronjs.org/docs/tutorial/security#12-disable-or-limit-navigation
+	contents.on('will-navigate', (event, _navigationUrl) => event.preventDefault());
+
+	// https://electronjs.org/docs/tutorial/security#13-disable-or-limit-creation-of-new-windows
+	contents.on('new-window', async (event, _navigationUrl) => event.preventDefault());
+
+	// https://electronjs.org/docs/tutorial/security#16-filter-the-remote-module
+	app.on('remote-get-builtin', (event, _webContents, _moduleName) => event.preventDefault());
+	app.on('remote-get-global', (event, _webContents, _moduleName) => event.preventDefault());
+	app.on('remote-get-current-window', (event, _webContents) => event.preventDefault());
+	app.on('remote-get-current-web-contents', (event, _webContents) => event.preventDefault());
+});
