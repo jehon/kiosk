de51863948bc25d353a9d7a36187a618a99d3044
Jean Honlet <jehon@users.noreply.github.com>
2020-01-12 22:11:13 +0100

    fix: clean up packages

--- bin/xsession-kiosk.sh
+++ bin/xsession-kiosk.sh
@@ -5,19 +5,19 @@
 xset +dpms
 # xset dpms 0 0 900
 
-shellcheck disable=SC1091
+# shellcheck disable=SC1091
 . /etc/profile.d/kiosk-profile.sh
 
-fixCrash() {
-	sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' "$1"
-	sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' "$1"
-}
+# fixCrash() {
+# 	sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' "$1"
+# 	sed -i 's/"exit_type":"Crashed"/"exit_type":"None"/' "$1"
+# }
 
-fixCrash "$HOME/.config/chromium/Default/Preferences"
-fixCrash "$HOME/.config/chromium/Local State"
+# fixCrash "$HOME/.config/chromium/Default/Preferences"
+# fixCrash "$HOME/.config/chromium/Local State"
 
 (
 	echo "********** Starting session kiosk ************************"
 	pushd "$KIOSK_APP" > /dev/null
-	/usr/bin/node main.js
+	npm run start
 ) 2>&1 | tee /tmp/kiosk-xsession.log
