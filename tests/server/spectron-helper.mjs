4b3fde776487b261c1af950f202dcfdd1ad8e342
Jean Honlet <jehon@users.noreply.github.com>
2020-01-16 17:00:55 +0100

    wip: spectron does not work

--- tests/server/spectron-helper.mjs
+++ tests/server/spectron-helper.mjs
@@ -7,4 +7,11 @@ const spectronApp = new Spectron.Application({
 	path: '../../main.js'
 });
 
+beforeAll(async () => {
+	console.log('Starting spectron app');
+	return spectronApp.start();
+});
+afterAll(async () => spectronApp.stop());
+
+
 export default spectronApp;
