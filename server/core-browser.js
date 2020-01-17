fc19f91449a3b7de01466c17da1580182086e94d
Jean Honlet <jehon@users.noreply.github.com>
2020-01-17 21:43:46 +0100

    feature: map for credentials in core-browser

--- server/core-browser.js
+++ server/core-browser.js
@@ -1,10 +1,34 @@
 
-const { BrowserWindow } = require('electron');
+const { app: electronApp, BrowserWindow } = require('electron');
 
 const logger = require('./server-logger.js')('core:browser');
 const devMode = require('./server-config.js')('core.devMode', false);
 
 let win;
+
+const credentialsMap = new Map();
+
+electronApp.on('login', (event, webContents, details, authInfo, callback) => {
+
+	// https://github.com/electron/electron/blob/master/docs/api/web-contents.md#event-login
+	// https://stackoverflow.com/questions/38281113/how-do-i-use-the-login-event-in-electron-framework
+
+	logger.debug(`login request for ${details.url}`);
+	for(const url of credentialsMap.keys()) {
+		const v = credentialsMap.get(url);
+		if (details.url.startsWith(url)) {
+			logger.debug(`Auto fill in credentials of ${details.url} for ${url} with ${v.username}`);
+			event.preventDefault();
+			callback(v.username, v.password);
+			break;
+		}
+	}
+});
+
+module.exports.registerCredentials = function(url, data) {
+	logger.debug(`Registering credentials for ${data.username}@${url}: #${data.password.length} characters`);
+	credentialsMap.set(url, data);
+};
 
 module.exports.start = async function() {
 	const opts = {
