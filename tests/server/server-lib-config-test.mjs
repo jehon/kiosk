a9d4deff71ad9421fb4f2137f0eb8c4183a1f621
Jean Honlet <jehon@users.noreply.github.com>
2020-11-02 18:00:00 +0100

    chore: migrate more

---
+++ tests/server/server-lib-config-test.mjs
@@ -0,0 +1,47 @@
+
+import serverAppFactory from '../../server/server-app.mjs';
+import getConfig, { setConfig, loadConfigFromFile, resetConfig } from '../../server/server-lib-config.mjs';
+import { fn } from './at-helper.mjs';
+
+const app = serverAppFactory('server');
+
+describe(fn(import.meta.url), () => {
+	let backupConfig;
+
+	beforeEach(() => {
+		backupConfig = getConfig();
+		setConfig('', {
+			test: {
+				a: 123
+			}
+		});
+	});
+
+	afterEach(() => {
+		setConfig('', backupConfig);
+	});
+
+	it('should read values', () => {
+		expect(getConfig().test.a).toBe(123);
+		expect(getConfig('test.a')).toBe(123);
+
+		expect(getConfig('test.b')).toBeUndefined();
+		expect(getConfig('test.b', 456)).toBe(456);
+
+		expect(getConfig('server.root')).not.toBeNull();
+	});
+
+	it('should get/Set', function () {
+		expect(getConfig('test.getset')).toBeUndefined();
+		expect(getConfig('test.getset', 456)).toBe(456);
+		setConfig('test.getset', 123);
+		expect(getConfig('test.getset')).toBe(123);
+	});
+
+	it('should read from file', async () => {
+		resetConfig();
+		await loadConfigFromFile(app, ['tests/kiosk.yml']);
+		expect(getConfig('server.root')).not.toBeNull();
+		expect(getConfig('server.devMode')).toBeFalse();
+	});
+});
