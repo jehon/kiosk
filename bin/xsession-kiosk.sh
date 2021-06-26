473e4224275142b08cd40887314ea45fc3c8eec3
Jean Honlet <jehon@users.noreply.github.com>
2021-06-26 20:24:37 +0200

    refactor: initial install

--- bin/xsession-kiosk.sh
+++ bin/xsession-kiosk.sh
@@ -5,10 +5,11 @@
 xset +dpms
 # xset dpms 0 0 900
 
-KIOSK_APP="$(dirname "$(dirname "$BASH_SOURCE" )" )"
+KIOSK_APP="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
+mkdir -p "$KIOSK_APP/tmp"
 
 (
 	echo "********** Starting session kiosk ************************"
-	pushd "$KIOSK_APP" > /dev/null
+	pushd "$KIOSK_APP" || exit 1 >/dev/null
 	npm start
-) 2>&1 | tee $HOME/kiosk-xsession.log
+) 2>&1 | tee "$KIOSK_APP"/tmp/kiosk-xsession.log
