4a2a6d1d6905e279bc8e7a54873528075defb7bd
Jean Honlet <jehon@users.noreply.github.com>
2021-07-02 21:03:10 +0200

    refactor: no bootstrap in photo-frame

--- packages/photo-frame/photo-frame-client.js
+++ packages/photo-frame/photo-frame-client.js
@@ -1,6 +1,4 @@
 
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
-
 import { ClientApp, ClientAppElement, forceGC } from '../../client/client-app.js';
 
 const app = new ClientApp('photo-frame');
@@ -54,9 +52,7 @@ class KioskPhotoFrame extends ClientAppElement {
 		super();
 
 		this.attachShadow({ mode: 'open' });
-		// See https://getbootstrap.com/docs/4.0/components/carousel/
 		this.shadowRoot.innerHTML = `
-		<link rel='stylesheet' type='text/css' href='../node_modules/bootstrap/dist/css/bootstrap.min.css'>
 		<style>
 			:host-context(body[inactive]) .hideOnInactive {
 				display: none;
@@ -67,46 +63,69 @@ class KioskPhotoFrame extends ClientAppElement {
 			}
 
 			#myCarousel {
+				position: relative;
+				width: 100%;
 				height: 100%;
-			}
-			.carousel-inner {
-				height: 100%;
-			}
-			.carousel-item {
-				height: 100%
-			}
-			.carousel-caption {
-				text-shadow: 1px 1px 2px black, 0 0 1em grey, 0 0 0.2em grey
 			}
 
 			/* image */
-			.data {
+			#myCarousel > #img {
+				width: 100%;
+				height: 100%;
+				object-fit: contain;
+			}
+
+			/* commands */
+			#overlay {
 				position: absolute;
 				top: 0;
 				left: 0;
-				color: white;
-				background-color: gray;
+
+				width: 100%;
+				height: 100%;
+
+				display: grid;
+
+				grid-template-areas:
+					"left . right"
+					"left center right"
+					"left bottom right";
+				grid-template-rows: 75% auto 10%;
+				grid-template-columns: 40px auto 40px;
+
+				justify-items: stretch;
 			}
 
-			/* thumbs */
+			#overlay > * {
+				place-self: center;
+				text-shadow: 1px 1px 2px black, 0 0 1em grey, 0 0 0.2em grey
+			}
 
-			.carousel-indicators {
+			#prev, #next {
+				font-size: 40px;
+			}
+
+			#infos {
+				text-align: center;
+				vertical-align: middle;
+			}
+
+			#thumbs {
+				grid-area: center;
+
+				position: absolute;
+
 				max-height: 50px;
 				margin-bottom: 0.5rem;
 			}
 
-			.thumb {
-				/* border: white solid 2px; */
-			}
-
-			.thumb.active {
+			#thumbs > .active {
 				background-color: gray;
 				border-radius: 2px;
 				border: solid 2px gray;
 			}
 
-			.carousel-item img,
-			.carousel-indicators img {
+			#thumbs > #thumbnail {
 				display: block;
 				height: 100%;
 				width: 100%;
@@ -114,91 +133,91 @@ class KioskPhotoFrame extends ClientAppElement {
 			}
 
 		</style>
-		<div id="myCarousel" class="carousel slide">
-			<!-- main content -->
-			<div class="carousel-inner" id="content"></div>
-
-			<!-- thumbs -->
-			<ol class="hideOnInactive carousel-indicators" id="thumbs"></ol>
-
-			<!-- carousel navigation -->
-			<a class="hideOnInactive carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="hideOnInactive carousel-control-next" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
+		<div id="myCarousel">
+			<img id="img" />
+			<div id="overlay">
+				<div style="grid-area: left"   id="prev"   class="hideOnInactive">&lt;</div>
+				<div style="grid-area: right"  id="next"   class="hideOnInactive">&gt;</div>
+				<div style="grid-area: bottom" id="infos"  ></div>
+				<div style="grid-area: center" id="thumbs" class="hideOnInactive">thumbs</div>
+			</div>
 		</div>`;
 
-		this.carousel = {
-			main: this.shadowRoot.querySelector('#myCarousel'),
-			// main$: $(this.carousel.main),
-			mainFn: (cmd) => $(this.carousel.main).carousel(cmd),
-			content: this.shadowRoot.querySelector('#content'),
-			thumbs: this.shadowRoot.querySelector('#thumbs'),
-			next: this.shadowRoot.querySelector('[data-slide="next"]'),
-			prev: this.shadowRoot.querySelector('[data-slide="prev"]')
-		};
+		this._carouselImg = /** @type {HTMLImageElement} */ (this.shadowRoot.querySelector('#img'));
+		this._carouselInfos = this.shadowRoot.querySelector('#infos');
+		this._carouselThumbs = this.shadowRoot.querySelector('#thumbs');
 
-		/* Next button */
-		this.carousel.next.addEventListener('click', () => {
-			next();
-		});
+		this.shadowRoot.querySelector('#prev').addEventListener('click', () => prev());
+		this.shadowRoot.querySelector('#next').addEventListener('click', () => next());
 
-		/* Previous button */
-		this.carousel.prev.addEventListener('click', () => {
-			prev();
-		});
-
-		/* global $ */
-		this.carousel.mainFn({
-			interval: false
+		this.addEventListener('wheel', (event) => {
+			event.preventDefault();
+			const d = event.deltaY;
+			if (d < 0) {
+				prev();
+			} else {
+				next();
+			}
 		});
 	}
 
-	// setServerState(status) {
-	// 	super.setServerState(status);
-	// 	this.adaptList();
-	// }
-
 	updateList() {
-		if (picturesList.length < 1) {
-			return;
-		}
-		this.carousel.content.innerHTML = '';
-		this.carousel.thumbs.innerHTML = '';
+		this._carouselThumbs.innerHTML = '';
+		if (picturesList.length >= 1) {
+			// TODO: manage thumbnails ! => need to generate them !
 
-		for (let i = 0; i < picturesList.length; i++) {
-			const v = picturesList[i];
-			// TODO: date legend: should be clean up for not significant numbers!
-			this.carousel.content.insertAdjacentHTML('beforeend',
-				`<div class="carousel-item " data-slide-number="${i}">
-					<pre class="debug data" >${JSON.stringify(v, null, 4)}</pre>
-					<img src="${v.url}">
-					<div class="hideOnInactive carousel-caption d-none d-md-block">
-						<h5>${v.data.comment}</h5>
-						<p>${v.data.date}</p>
-					</div>
-				</div>`);
+			// this.carousel.content.innerHTML = '';
+			// this.carousel.thumbs.innerHTML = '';
 
-			this.carousel.thumbs.insertAdjacentHTML('beforeend',
-				`<div class="thumb" data-target="#myCarousel" data-slide-to="${i}">
-					<img src="${v.url}?thumb=1&height=50">
-				</div>`);
+			// for (let i = 0; i < picturesList.length; i++) {
+			// 	const v = picturesList[i];
+			// 	// TODO: date legend: should be clean up for not significant numbers!
+			// 	this.carousel.content.insertAdjacentHTML('beforeend',
+			// 		`<div class="carousel-item " data-slide-number="${i}">
+			// 			<pre class="debug data" >${JSON.stringify(v, null, 4)}</pre>
+			// 			<img src="${v.url}">
+			// 			<div class="hideOnInactive carousel-caption d-none d-md-block">
+			// 				<h5>${v.data.comment}</h5>
+			// 				<p>${v.data.date}</p>
+			// 			</div>
+			// 		</div>`);
+
+			// 	this.carousel.thumbs.insertAdjacentHTML('beforeend',
+			// 		`<div class="thumb" data-target="#myCarousel" data-slide-to="${i}">
+			// 			<img src="${v.url}?thumb=1&height=50">
+			// 		</div>`);
+			// }
+			// this.carousel.content.querySelector('[data-slide-number="0"]').classList.add('active');
+			// this.carousel.thumbs.querySelector('[data-slide-to="0"]').classList.add('active');
+			// this.carousel.thumbs.querySelectorAll('[data-slide-to]').forEach(el =>
+			// 	el.addEventListener('click', () => this.carousel.mainFn(parseInt(el.dataset.slideTo))));
 		}
-		this.carousel.content.querySelector('[data-slide-number="0"]').classList.add('active');
-		this.carousel.thumbs.querySelector('[data-slide-to="0"]').classList.add('active');
-		this.carousel.thumbs.querySelectorAll('[data-slide-to]').forEach(el =>
-			el.addEventListener('click', () => this.carousel.mainFn(parseInt(el.dataset.slideTo))));
+
+		this.updatePicture();
 	}
 
 	updatePicture() {
-		if (!this.carousel) {
+		if(!this._carouselImg) {
 			return;
 		}
-		this.carousel.mainFn(pictureIndex);
+
+		let photo = {
+			url: '../packages/photo-frame/photo-frame.png',
+			data: {
+				comment: 'no picture',
+				date: 'today'
+			}
+		};
+
+		if (picturesList.length > 0) {
+			photo = picturesList[pictureIndex];
+		}
+
+		this._carouselInfos.innerHTML = `${photo.data.comment ?? ''}<br>${('' + (photo.data.date ?? '')).substring(0, 10)}`;
+
+		this._carouselImg.src = photo.url;
+
+
 		setTimeout(() => forceGC(), 1000);
 	}
 }
