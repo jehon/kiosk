9332874224aeeab6c31e5a97bf7f11453ecc77f6
Jean Honlet <jehon@users.noreply.github.com>
2021-07-03 15:44:50 +0200

    refactor: sync under kiosk
    and update dependencies on startup

--- bin/xsession-kiosk.sh
+++ bin/xsession-kiosk.sh
@@ -8,7 +8,11 @@ xset +dpms
 KIOSK_APP="$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 mkdir -p "$KIOSK_APP/tmp"
 
+truncate --size 0 "$KIOSK_APP"/tmp/kiosk-xsession.log
 (
+	echo "********** Checking setup ************************"
+	"$KIOSK_APP"/bin/kiosk-upgrade-sources-dependencies.sh
+
 	echo "********** Starting session kiosk ************************"
 	pushd "$KIOSK_APP" || exit 1 >/dev/null
 	npm start
