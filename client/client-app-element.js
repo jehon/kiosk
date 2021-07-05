9dff3b2f2b887be7781b7b873e75ecb0f505dde6
Jean Honlet <jehon@users.noreply.github.com>
2021-07-05 18:00:00 +0200

    feat: do ClientAppElement is auto unsubscribe

--- client/client-app-element.js
+++ client/client-app-element.js
@@ -1,5 +1,35 @@
 
 export default class ClientAppElement extends HTMLElement {
+    unregister = [];
+
+    constructor(app) {
+        super();
+        this.app = app;
+    }
+
+    connectedCallback() {
+        // Unregister potentially connected stuff...
+        this.disconnectedCallback();
+
+        this.addUnregister(
+            this.app.onServerStateChanged(
+                (status) => this.setServerState(status)
+            )
+        );
+    }
+
+    disconnectedCallback() {
+        for (const fn of this.unregister) {
+            fn();
+        }
+        this.unregister.length = 0;
+    }
+
+    addUnregister(u) {
+        this.unregister.push(u);
+        return this;
+    }
+
     setServerState(state) {
         this.status = state;
     }
