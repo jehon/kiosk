a139c2e2d7e5f0183bdf69f62981502ba49af012
Jean Honlet <jehon@users.noreply.github.com>
2021-07-14 06:42:24 +0200

    feat: remove toastr of raspberrypi

--- bin/kiosk-install-sudo.sh
+++ bin/kiosk-install-sudo.sh
@@ -5,5 +5,9 @@ apt install \
     unclutter \
     gcc g++ make
 
+# remove the toastr warning about battery
+# (we still have the lightnight bolt to warn about power)
+apt remove lxplug-ptbatt
+
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt --yes install nodejs
