cac6b437942bbcbbba4342bd7a4d7cf6b79fdf72
Jean Honlet <jehon@users.noreply.github.com>
2021-07-14 18:00:00 +0200

    feat: use snap version of node

--- bin/kiosk-install-sudo.sh
+++ bin/kiosk-install-sudo.sh
@@ -3,11 +3,15 @@
 apt install \
     ffmpeg exiv2 gettext-base \
     unclutter \
-    build-essential
+    build-essential \
+    snapd
 
 # remove the toastr warning about battery
 # (we still have the lightnight bolt to warn about power)
 apt remove lxplug-ptbatt
 
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-apt --yes install nodejs
+# curl -sL https://deb.nodesource.com/setup_current.x | bash -
+# apt --yes install nodejs
+
+snap install core
+snap install node --classic
