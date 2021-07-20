592e66231b075e489b726f49c3c0990f021950b7
Jean Honlet <jehon@users.noreply.github.com>
2021-07-20 18:00:00 +0200

    chore: add code to reset memory "just in case"

--- client/client-server.js
+++ client/client-server.js
@@ -17,3 +17,11 @@ export function sendLogToServer(data) {
 export function sendToServer(channel, data) {
     ipcRenderer.send(channel, data);
 }
+
+// /**
+//  * Reset cache.
+//  * See http://seenaburns.com/debugging-electron-memory-usage/
+//  */
+// import TimeInterval from '../common/TimeInterval.js';
+// const { webFrame } = require('electron');
+// new TimeInterval(() => webFrame.clearCache(), 2 * 60 * 1000).start();
