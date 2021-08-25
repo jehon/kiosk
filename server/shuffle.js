34b6d9a7d804f8eaed5f30ef231ddb8a89083019
Jean Honlet <jehon@users.noreply.github.com>
2021-08-25 18:00:00 +0200

    feat: shuffle

---
+++ server/shuffle.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// import secureRandom from 'secure-random-uniform';
+
+/**
+ * @param {object} weightedList with weight
+ * @returns {any} taken in random
+ */
+function takeOne(weightedList) {
+
+    const sum = Object.values(weightedList).reduce((n, i) => n + i, 0);
+
+    if (sum == 0) {
+        return Object.keys(weightedList).pop();
+    }
+
+    const r = Math.random() * sum;
+    // const r = secureRandom(sum);
+
+    let s = 0;
+    for (const k of Object.keys(weightedList)) {
+        s += weightedList[k];
+        if (r < s) {
+            return k;
+        }
+    }
+
+    return Object.keys(weightedList).pop();
+}
+
+
+/**
+ * @param {object} weightedList with weight
+ * @returns {Array} weighted
+ */
+export default function shuffle(weightedList) {
+    const keys = Object.keys(weightedList);
+
+    const res = [];
+    const N = keys.length;
+    for (var i = 0; i < N; i++) {
+        const k = takeOne(weightedList);
+        delete weightedList[k];
+        res.push(k);
+    }
+
+    return res;
+}
