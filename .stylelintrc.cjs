ddcf7381287ab5e8009be665fe6aa1979c3479f8
Jean Honlet <jehon@users.noreply.github.com>
2021-11-23 21:34:59 +0100

    style: simplify

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     "selector-type-no-unknown": null,
     "no-descending-specificity": null,
     "declaration-empty-line-before": null,
-    "declaration-colon-space-after": null
+    "declaration-colon-space-after": null,
+    "no-empty-first-line": null
   }
 }
