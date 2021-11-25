008b7adbc21877b48eaedaf31033b684a6123309
Jean Honlet <jehon@users.noreply.github.com>
2021-11-25 18:00:00 +0100

    feat(wip): music

--- packages/music/music-client.js
+++ packages/music/music-client.js
@@ -1,33 +1,27 @@
 
 import ClientAppElement from '../../client/client-app-element.js';
-import { ClientApp } from '../../client/client-app.js';
+import { ClientApp, iFrameBuilder } from '../../client/client-app.js';
 
 const app = new ClientApp('music');
 
-export class KioskMusic extends ClientAppElement {
+export class KioskMusicClient extends ClientAppElement {
 	constructor() {
 		super(app);
-		this.innerHTML = `
+
+		this.attachShadow({ mode: 'open' });
+		this.shadowRoot.innerHTML = `
 			music
 		`;
-	}
-
-	setServerState(status) {
-		super.setServerState(status);
-		this.adapt();
-	}
-
-	adapt() {
-	}
-
-	connectedCallback() {
+		this.shadowRoot.insertAdjacentElement('afterbegin', new iFrameBuilder('http://192.168.1.9:4001'));
 	}
 }
 
-customElements.define('kiosk-music', KioskMusic);
+customElements.define('kiosk-music', KioskMusicClient);
 
 app
-	.setMainElementBuilder(() => new KioskMusic())
-	.menuBasedOnIcon('../packages/music/icon.svg');
+	.setMainElementBuilder(() => new KioskMusicClient())
+	.menuBasedOnIcon('../packages/music/icon.svg')
+	// .setPriority(10000) // Temp
+	;
 
 export default app;
