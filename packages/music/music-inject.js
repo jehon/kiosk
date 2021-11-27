05dd8ec9aba2304165aa8ef65cc674b0c1a05ef6
Jean Honlet <jehon@users.noreply.github.com>
2021-11-27 13:01:01 +0100

    feat(music): wip about login

--- packages/music/music-inject.js
+++ packages/music/music-inject.js
@@ -1,2 +1,37 @@
 
-console.info('kiosk is there !');
+console.debug('kiosk is there !'); // eslint-disable-line 
+
+/**
+ * @param {string} username of the user
+ * @param {string} password of the user
+ */
+function doLogin(username, password) {
+    return new Promise((resolve) => {
+        const loginInterval = setInterval(() => {
+            let i = 0;
+
+            document.querySelectorAll('input#login_username').forEach(el => {
+                console.debug('Found username: ', el); // eslint-disable-line 
+                el.value = username;
+                i = i | 1;
+            });
+
+            document.querySelectorAll('input#login_passwd').forEach(el => {
+                console.debug('Found password: ', el); // eslint-disable-line 
+                el.value = password;
+                i = i | 2;
+            });
+
+            if (i == 3) {
+                document.querySelectorAll('#login-btn button').forEach(el => {
+                    console.debug('Found button: ', el); // eslint-disable-line 
+                    clearInterval(loginInterval);
+                    el.click();
+                    resolve();
+                });
+            }
+        }, 2 * 1000);
+    });
+}
+doLogin();
+
