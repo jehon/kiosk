8396ea844fe0f36929a07bf4970415b49e686002
Jean Honlet <jehon@users.noreply.github.com>
2021-12-01 18:00:00 +0100

    refactor: client-app-element-webview

--- packages/music/music-client.js
+++ packages/music/music-client.js
@@ -1,45 +1,15 @@
 
-import ClientAppElement from '../../client/client-app-element.js';
+import KioskClientElementWebView from '../../client/client-app-element-webview.js';
 import { ClientApp } from '../../client/client-app.js';
-import { sendToServer } from '../../client/client-server.js';
 import { priorities } from '../../client/config.js';
 
 const app = new ClientApp('music');
 
-export class KioskMusicClient extends ClientAppElement {
-	#top;
-
-	constructor() {
-		super(app);
-
-		this.attachShadow({ mode: 'open' });
-		this.shadowRoot.innerHTML = `
-			<div id='top'></div>
-		`;
-		this.#top = this.shadowRoot.querySelector('#top');
-	}
-
-	connectedCallback() {
-		super.connectedCallback();
-		sendToServer('music', { active: true });
-		this.#top.innerHTML = 'Loading';
-
-		// When active, it remain active for all the time needed...
-		app.setPriority(priorities.music.elevated);
-	}
-
-	disconnectedCallback() {
-		super.disconnectedCallback();
-		sendToServer('music', { active: false });
-		this.#top.innerHTML = 'Disconnected';
-		app.setPriority(priorities.music.normal);
-	}
-}
-
-customElements.define('kiosk-music', KioskMusicClient);
-
 app
-	.setMainElementBuilder(() => new KioskMusicClient())
+	.setMainElementBuilder(() =>
+		(new KioskClientElementWebView(app))
+			.withElevatedPriority(priorities.music.elevated)
+	)
 	.menuBasedOnIcon('../packages/music/icon.svg')
 	.setPriority(priorities.music.normal)
 	;
