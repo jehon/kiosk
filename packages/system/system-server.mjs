cc18edb2d9756dff83cc483226c56a8fd0db8358
Jean Honlet <jehon@users.noreply.github.com>
2022-02-15 21:29:19 +0100

    feat: system with ip's list

--- packages/system/system-server.mjs
+++ packages/system/system-server.mjs
@@ -1,5 +1,8 @@
 
+import { ACTIVITY_SUB_CHANNEL } from '../../common/constants.js';
 import serverAppFactory from '../../server/server-app.js';
+import { onClient } from '../../server/server-lib-gui.js';
+import os from 'os';
 
 /**
  * @type {module:server/ServerApp}
@@ -8,14 +11,46 @@ const app = serverAppFactory('system');
 export default app;
 
 /**
+ * Get the list of all the ips
+ *
+ * @returns {Array<string>} as external ip's
+ */
+function getNetworkIP() {
+    const nets = os.networkInterfaces();
+    const ips = [];
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            if (!net.internal) {
+                ips.push(net.address);
+            }
+        }
+    }
+    return ips;
+}
+
+/**
  * Initialize the package
  *
  * @returns {module:server/ServerApp} the app
  */
 export async function init() {
+    const channel = app.name + ACTIVITY_SUB_CHANNEL;
+
     app.setState({
-        hello: 'world'
+        initial: true
     });
+
+    onClient(channel, (status) => {
+
+        os;
+        if (status.active) {
+            app.setState({
+                time: new Date(),
+                ips: getNetworkIP(),
+            });
+        }
+    });
+
 
     return app;
 }
