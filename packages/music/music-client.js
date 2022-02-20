89386afa6ebf9ced561237a0a1e671da3544d3ec
Jean Honlet <jehon@users.noreply.github.com>
2022-02-20 20:59:21 +0100

    feat(mpd): switch to new mode

--- packages/music/music-client.js
+++ packages/music/music-client.js
@@ -1,15 +1,26 @@
 
-import KioskWebviewElement from '../../client/client-element-webview.js';
 import { ClientApp } from '../../client/client-app.js';
 import { priorities } from '../../client/config.js';
+import ClientElement from '../../client/client-element.js';
 
 const app = new ClientApp('music');
 
+export class KioskMusicElement extends ClientElement {
+	ready() {
+		this.shadowRoot.innerHTML = `
+		musique!
+			<iframe 
+				src='http://localhost:8800'
+				width='100%'
+				height='100%'
+			></iframe>
+		`;
+	}
+}
+customElements.define('kiosk-music-element', KioskMusicElement);
+
 app
-	.setMainElementBuilder(() =>
-		(new KioskWebviewElement())
-			.withActivePriority(priorities.music.elevated)
-	)
+	.setMainElementBuilder(() => new KioskMusicElement())
 	.menuBasedOnIcon('../packages/music/icon.svg')
 	.setPriority(priorities.music.normal)
 	;
--- packages/music/music-server.mjs
+++ packages/music/music-server.mjs
@@ -1,17 +1,5 @@
 
-// https://www.npmjs.com/package/syno
-// https://global.download.synology.com/download/Document/Software/DeveloperGuide/Os/DSM/All/enu/DSM_Login_Web_API_Guide_enu.pdf
-// https://www.nas-forum.com/forum/topic/46256-script-web-api-synology/
-// https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package/AudioStation/All/enu/AS_Guide.pdf
-
-// https://myds.com:port/webapi/entry.cgi?api=SYNO.API.Auth&version=6&method=login&account=<USERNAME>&passwd=<PASSWORD></PASSWORD>
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-import { readFileSync } from 'original-fs';
 import serverAppFactory from '../../server/server-app.js';
-import hookWebview from '../../server/server-app-webview.js';
 import child_process from 'child_process';
 import { onClient } from '../../server/server-lib-gui.js';
 
@@ -25,10 +13,6 @@ export default app;
 const status = {
 	config: app.getConfig(),
 };
-
-const server = app.getConfig('credentials.synology.url');
-const username = app.getConfig('credentials.synology.username');
-const password = app.getConfig('credentials.synology.password');
 
 const MPDServerCommand = 'externals/websockify/run 8800 localhost:6600 --web externals/mpd/';
 
@@ -82,14 +66,6 @@ export function init() {
 			stopProxy();
 		}
 	}));
-
-	// Old behavior
-	const script = readFileSync(path.join(path.dirname(fileURLToPath(import.meta.url)), 'music-inject.js'));
-	hookWebview(
-		app,
-		`${server}/?launchApp=SYNO.SDS.AudioStation.Application`,
-		`${script}; doLogin("${username}", "${password}");`
-	);
 
 	app.setState(status);
 
