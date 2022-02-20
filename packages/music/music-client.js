dff113b8b9c259420b7f2f7b29b228fee1940ce9
Jean Honlet <jehon@users.noreply.github.com>
2022-02-20 21:35:50 +0100

    refactor: use iFrameBuilder

--- packages/music/music-client.js
+++ packages/music/music-client.js
@@ -1,26 +1,11 @@
 
-import { ClientApp } from '../../client/client-app.js';
+import { ClientApp, iFrameBuilder } from '../../client/client-app.js';
 import { priorities } from '../../client/config.js';
-import ClientElement from '../../client/client-element.js';
 
 const app = new ClientApp('music');
 
-export class KioskMusicElement extends ClientElement {
-	ready() {
-		this.shadowRoot.innerHTML = `
-		musique!
-			<iframe 
-				src='http://localhost:8800'
-				width='100%'
-				height='100%'
-			></iframe>
-		`;
-	}
-}
-customElements.define('kiosk-music-element', KioskMusicElement);
-
 app
-	.setMainElementBuilder(() => new KioskMusicElement())
+	.setMainElementBuilder(() => iFrameBuilder('http://localhost:8800'))
 	.menuBasedOnIcon('../packages/music/icon.svg')
 	.setPriority(priorities.music.normal)
 	;
