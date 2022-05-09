93ed24858b06d0dcd01bf1ffdfa2362a45a05182
Jean Honlet <jehon@users.noreply.github.com>
2022-05-09 17:28:58 +0200

    refactor: use index.json for image index

--- packages/photo-frame/photo-frame-server.mjs
+++ packages/photo-frame/photo-frame-server.mjs
@@ -2,32 +2,12 @@
 import serverAppFactory from '../../server/server-app.js';
 
 import fs from 'fs';
-import mime from 'mime-types';
 import path from 'path';
-import minimatch from 'minimatch';
-import exifParser from './exif-parser.mjs';
-import shuffle, { shuffleArray } from '../../server/shuffle.js';
-
-/**
- * @typedef FolderConfig
- * @param {string} name - name of the config
- * @param {string} folder relative or absolute
- * @param {number} quantity of files
- * @param {string} mimeTypePattern to be matched
- * @param {Array<string>} excludes to be excluded (using minimatch)
- *
- *  folder: /media/photo/
- *	quantity: 20
- *	excludes:
- *	  - "#recycle"
- *	  - "0 A trier"
- */
 
 /**
  * @typedef ImageData
  * @param {string} subPath relative to the folderConfig home
  * @param {string} path is subPath concatenated with folderConfig path
- * @param {string} url the url of the file (unused ?)
  * @param {FolderConfig} folderConfig where the file has been defined
  * @param {string} data.title from exiv
  * @param {string} data.date from exiv
@@ -50,297 +30,6 @@ const app = serverAppFactory('photo-frame');
 
 export default app;
 
-// See https://nodejs.org/dist/latest-v12.x/docs/api/modules.html#modules_module_createrequire_filename
-import pLimitFactory from 'p-limit';
-
-const buildingLogger = app.childLogger('building');
-
-/**
- * Test if a file match the pattern
- * (used to exclude files)
- *
- * @param {string} filename to be matched
- * @param {string} pattern to match
- * @returns {boolean} true if it match
- */
-function matchFile(filename, pattern) {
-	return minimatch(filename, pattern, {
-		nocase: true,
-		nocomment: true,
-		nonegate: true
-	});
-}
-
-/**
- * Get all files of a folder
- *
- * @param {string} folder relative to cwd or absolute
- * @param {Array<string>} excludes to be excluded (by minimatch *.*, ...)
- * @returns {Array<string>} of file paths relative to folderConfig.folder
- */
-export async function _getFilesFromFolder(folder, excludes = []) {
-	return fs.readdirSync(folder)
-		.filter(file => !(file in ['.', '..']))
-		.filter(file => excludes.reduce((acc, val) => acc && !matchFile(file, val), true));
-}
-
-/**
- * Get files of mimetype in a folder
- *
- * @param {string} folder relative to cwd or absolute
- * @param {Array<string>} excludes to be excluded (by minimatch *.*, ...)
- * @param {string} mimeTypePattern to filter in (image/*)
- * @returns {Array<string>} of file paths relative to folderConfig.folder
- */
-export async function _getFilesFromFolderByMime(folder, excludes, mimeTypePattern) {
-	return (await _getFilesFromFolder(folder, excludes))
-		.filter(f => {
-			let mt = mime.lookup(path.join(folder, f));
-			if (typeof (mt) != 'string') {
-				return false;
-			}
-			return mt.match(mimeTypePattern);
-		});
-}
-
-/**
- * Get subfolders out of a folder
- *
- * @param {string} folder relative to cwd or absolute
- * @param {Array<string>} excludes to be excluded (by minimatch *.*, ...)
- * @returns {Array<string>} of folders (absolute)
- */
-export async function _getFoldersFromFolder(folder, excludes) {
-	return (await _getFilesFromFolder(folder, excludes))
-		.filter(f => fs.statSync(path.join(folder, f)).isDirectory());
-}
-
-/**
- * Find "n" files in the folders and build up a list
- * It will recurse to subfolders (up and down) until "n" files are found
- *
- * @param {FolderConfig} folderConfig where to search for
- * @param {number} n of files to take (will be updated with really taken count)
- * @param {string} subpath where to look for
- * @param {Array<string>} previouslySelected is the list of previously visited folder (relative to folderConfig)
- * @returns {Array<string>} is a list of files relative to folderConfig.folder
- */
-export async function _generateListingForPath(folderConfig, n = folderConfig.quantity, subpath = '', previouslySelected = []) {
-	buildingLogger.debug(folderConfig.name, '3.0 - getFilesFromFolderPath: ', subpath, folderConfig);
-
-	//
-	// Shuffle will send back an array of strings
-	//   each string is the name of a folder (relative to folderConfig.folder)
-	//
-	const priorityFolders = {
-		'.': 1,
-		...(await _getFoldersFromFolder(path.join(folderConfig.folder, subpath), folderConfig.excludes))
-			.reduce((acc, v) => {
-				acc[v] = 1;
-
-				try {
-					const dfile = path.join(folderConfig.folder, v, 'kiosk.json');
-					if (fs.statSync(dfile)) {
-						const content = JSON.parse(fs.readFileSync(dfile));
-						if (content && content.priority) {
-							buildingLogger.debug('Override by file for', dfile, ': ', JSON.stringify(content));
-							acc[v] = content.priority;
-						}
-					}
-				} catch (_e) {
-					// expected
-				}
-				return acc;
-			}, {})
-	};
-	buildingLogger.debug('3.0.1 Resolved folders', priorityFolders);
-
-	//
-	// We need an object here
-	// so that we can influence proportions of each times
-	//   key: folder name (relative to folderConfig.folder)
-	//   value: # of times this folder is taken into account in lottery
-	//            (once taken, it is removed)
-	//          default = 1
-	//
-	const folders = shuffle(priorityFolders);
-	buildingLogger.debug('3.0.2 Shuffled folders', folders);
-
-	const listing = [];
-
-	while (folders.length > 0 && listing.length < n) {
-		// Take the first one (top priority)
-		const f = folders.shift();
-
-		// Special case: we take the pictures
-		if (f == '.') {
-			if (previouslySelected.includes(subpath)) {
-				// Don't take twice the same folder
-				continue;
-			}
-			previouslySelected.push(subpath);
-
-			buildingLogger.debug(folderConfig.name, '# 3.1 - getFilesFromFolderPath: selecting pictures');
-
-			/** @type {Array<string>} - list of max(n) filename with correct mimetype */
-			const images = shuffleArray(await _getFilesFromFolderByMime(
-				path.join(folderConfig.folder, subpath),
-				folderConfig.excludes,
-				folderConfig.mimeTypePattern
-			));
-
-			/** @type {Array<string>} - list of filepath relative to folderConfig */
-			const imgList = images
-				.slice(0, Math.min(n,
-					images.length,
-					n - listing.length))
-				.map(filename => path.join(subpath, filename));
-			buildingLogger.debug(folderConfig.name, '# 3.2 - getFilesFromFolderPath: selecting pictures: ', imgList.length, imgList);
-
-			listing.push(...imgList);
-			continue;
-		}
-
-		// Take folders
-		buildingLogger.debug(folderConfig.name, '# 3.3 - getFilesFromFolderPath: selecting folder', f);
-		listing.push(...(await _generateListingForPath(
-			folderConfig,
-			n - listing.length,
-			path.join(subpath, f),
-			previouslySelected
-		)));
-	}
-	listing.sort();
-	return listing;
-}
-
-/**
- * To limit exiv execution to 1 at a a time
- */
-const exifReaderLimiter = pLimitFactory(1);
-
-/**
- * Build the whole list for a folderConfig
- *
- * @param {FolderConfig} folderConfig of a folder
- * @returns {Array<object>} list of files relative to folder root
- */
-export async function _generateListingForTopFolder(folderConfig) {
-	buildingLogger.debug(folderConfig.name, '# 2.0 - generateListingForTopFolder: given options: ', folderConfig);
-
-	// Initialize config
-	folderConfig = {
-		excludes: [],
-		mimeTypePattern: ['image/*'],
-		quantity: 10,
-		...app.getConfig('.folder-defaults', {}),
-		...folderConfig,
-	};
-
-	// if (folderConfig.folder[0] != '/') {
-	// 	folderConfig.folder = path.join(app.getConfig('server.root'), folderConfig.folder);
-	// }
-
-	buildingLogger.debug(folderConfig.name, '# 2.1 - generateListingForTopFolder: resolved options: ', folderConfig);
-	try {
-		fs.statSync(folderConfig.folder);
-	} catch (_e) {
-		app.error(`Could not find folder '${folderConfig.folder}'`);
-		return [];
-	}
-
-	/**
-	 * @type {Array<string>} of filenames
-	 */
-	let selectedFolderPictures = await _generateListingForPath(folderConfig);
-
-	// Enrich the data
-	buildingLogger.debug(folderConfig.name, '# 2.3 Extracting exif data for files', selectedFolderPictures);
-
-	/**
-	 * @type {Array<ImageData>} but without exiv data
-	 */
-	selectedFolderPictures = selectedFolderPictures
-		.map(filepath => ({
-			subPath: filepath,
-			path: path.join(folderConfig.folder, filepath),
-			folderConfig,
-		}))
-		.map(imageData => {
-			imageData.url = encodeURI(imageData.path);
-			if (imageData.url[0] != '/') {
-				// Relative to
-				//  - in "file" mode, relative to the index.html
-				//  - in "server" mode, relative to the CWD
-				imageData.url = '../' + imageData.url;
-			}
-			return imageData;
-		});
-
-	/**
-	 * @type {Array<ImageData>} with exiv data
-	 */
-	selectedFolderPictures = await Promise.all(
-		selectedFolderPictures.map(
-			fileObject => exifReaderLimiter(() => exifParser(fileObject.path))
-				.catch(e => { app.debug('Could not read exif: ', e); return {}; })
-				.then(data => ({
-					// TODO: what other information do we need?
-					...fileObject,
-					data
-				}))
-		));
-	buildingLogger.debug(folderConfig.name, '# 2.4 Extracting exif data done');
-
-	return selectedFolderPictures;
-}
-
-
-/**
- * Make the full selection of the images
- * and store it in the state
- *
- * @param {object} _data to filter the results
- * @returns {object} new state
- */
-export async function generateListing(_data = null) {
-	app.debug('0. Generate listing');
-	let selectedPictures = [];
-
-	// TODO: manage the data selection
-	const folders = app.getConfig('.folders', []);
-	buildingLogger.debug('1 - generateListing: Found top folders', folders);
-	for (const i in folders) {
-		const f = folders[i];
-		buildingLogger.debug(f.name, ' # 1.1 - generateListing using folder', f);
-		selectedPictures = selectedPictures.concat(await _generateListingForTopFolder(f));
-	}
-
-	// Sort the images
-	selectedPictures.sort((a, b) => {
-		if (!a.data.date && !b.data.date) {
-			return 0;
-		} else if (!a.data.date) {
-			return -1;
-		} else if (!b.data.date) {
-			return 1;
-		}
-		return a.data.date < b.data.date ? -1 : a.data.date == b.data.date ? 0 : 1;
-	});
-
-	buildingLogger.debug('0.2 - Updating listing to', selectedPictures);
-
-	const newState = {
-		hasList: true,
-		listing: selectedPictures
-	};
-
-	app.setState(newState);
-
-	app.debug('0.3 - Generating listing done', newState);
-	return newState;
-}
-
 /**
  * 
  * index.json: generated by file-selector:
@@ -353,7 +42,26 @@ export async function generateListing(_data = null) {
  * 
  */
 
+export async function loadList(folder) {
+	let listing = [];
+	try {
+		const indexFile = path.join(folder, 'index.json');
+		const txt = fs.readFileSync(indexFile);
+		listing = JSON.parse(txt).map(v => ({
+			...v,
+			url: path.join('..', folder, v.subPath)
+		}));
+	} catch (e) {
+		app.error(`Could not load from ${folder}`);
+		// ok
+	}
+	app.setState({
+		hasList: listing.length > 0,
+		listing
+	});
 
+	return listing;
+}
 
 /**
  * Initialize the package
@@ -366,20 +74,8 @@ export function init() {
 		listing: []
 	});
 
-	// We check that we have a list
-	app.addTimeInterval(async () => {
-		if (!app.getState().hasList) {
-			await generateListing();
-		}
-	}, app.getConfig('.checkListEverySeconds', 5 * 60));
+	loadList(app.getConfig('.folder'));
 
-	// // Refresh the list sometimes
-	// // On reboot :-)
-	// const refreshSchedule = app.getConfig('.refresh-cron', '0 0 5 * * *');
-	// app.debug('Programming resfresh at', refreshSchedule);
-	// app.cron((data) => generateListing(data), refreshSchedule);
-
-	generateListing();
 	return app;
 }
 
--- tests/server/package-photo-frame-server-test.mjs
+++ tests/server/package-photo-frame-server-test.mjs
@@ -1,25 +1,14 @@
 
-import path from 'path';
-
-import app, {
+import {
 	init,
-	_getFilesFromFolder,
-	_getFilesFromFolderByMime,
-	_getFoldersFromFolder,
-	_generateListingForPath,
-	_generateListingForTopFolder,
-	generateListing
+	loadList
 } from '../../packages/photo-frame/photo-frame-server.mjs';
 import getConfig, { setConfig } from '../../server/server-lib-config.js';
 
 import { fn } from './helper-main.mjs';
 
 const testFolderConfig = {
-	'name': 'photo',
-	'folder': 'tests/server/data/photo-frame',
-	'quantity': 5,
-	'mimeTypePattern': 'image/*',
-	'excludes': []
+	'folder': 'tests/server/data/photos',
 };
 
 describe(fn(import.meta.url), () => {
@@ -29,127 +18,31 @@ describe(fn(import.meta.url), () => {
 	});
 
 	beforeEach(() => {
-		setConfig('photo-frame', JSON.parse(JSON.stringify({
-			'folders': {
-				'photo': testFolderConfig
-			}
-		})));
+		setConfig('photo-frame', JSON.parse(JSON.stringify(testFolderConfig)));
 	});
 
 	afterAll(() => {
 		setConfig('', beforeConfig);
 	});
 
-	it('should _getFilesFromFolder', async function () {
-		expect(await _getFilesFromFolder(testFolderConfig.folder)).toEqual(['f1', 'f2']);
-		expect(await _getFilesFromFolder(testFolderConfig.folder, ['f1'])).toEqual(['f2']);
-	});
-
-	it('should _getFoldersFromFolder', async function () {
-		expect(await _getFoldersFromFolder(testFolderConfig.folder, [])).toEqual(['f1', 'f2']);
-	});
-
-	it('should _getFilesFromFolderByMime', async function () {
-		expect(await _getFilesFromFolderByMime(testFolderConfig.folder)).toEqual([]);
-		expect(await _getFilesFromFolderByMime(
-			path.join(testFolderConfig.folder, 'f1'),
-			[],
-			'image/*'
-		)).toEqual(['i1.png', 'i2.jpg', 'i4.jpg']);
-	});
-
-	it('should _generateListingForPath', async function () {
-		expect(await _generateListingForPath(testFolderConfig, 10, 'f1'))
-			.toEqual([
-				'f1/i1.png',
-				'f1/i2.jpg',
-				'f1/i4.jpg'
-			]);
-
-		expect(await _generateListingForPath(testFolderConfig, 10, '', ['f2']))
-			.toEqual([
-				'f1/i1.png',
-				'f1/i2.jpg',
-				'f1/i4.jpg'
-			]);
-
-		expect(await _generateListingForPath(testFolderConfig, 100))
-			.toEqual([
-				'f1/i1.png',
-				'f1/i2.jpg',
-				'f1/i4.jpg',
-				'f2/m1.jpg',
-				'f2/m2.jpg',
-				'f2/m3.jpg',
-				'f2/m4.jpg'
-			]);
-	});
-
-	it('should _generateListingForTopFolder', async function () {
-		expect((await _generateListingForTopFolder({
-			...testFolderConfig,
-			quantity: 100
-		})).length)
-			.toBe(7);
-
-		// Only a few
-		expect((await _generateListingForTopFolder({
-			...testFolderConfig,
-			quantity: 2
-		})).length)
-			.toBe(2);
-
-		// Accross two folders minimum
-		expect((await _generateListingForTopFolder({
-			...testFolderConfig,
-			quantity: 5
-		})).length)
-			.toBe(5);
-	});
-
-	it('should _generateListingForTopFolder with correct properties', async function () {
-		const cfg = {
-			...testFolderConfig,
-			quantity: 100
-		};
-		const res0 = (await _generateListingForTopFolder(cfg))[0];
-
-		expect(res0)
-			.toEqual({
-				subPath: 'f1/i1.png',
-				path: 'tests/server/data/photo-frame/f1/i1.png',
-				url: '../tests/server/data/photo-frame/f1/i1.png', // UNUSED
-				data: {
-					title: 'Test title here',
-					date: '2019-07-01 02:03:04',
-					orientation: 0
-				},
-				folderConfig: {
-					...testFolderConfig,
-					quantity: 100
-				}
-			});
-	});
-
-	it('should generateListing', async function () {
-		const newState = await generateListing();
-		expect(newState.hasList).toBeTrue();
-		expect(newState.listing.length).toBeGreaterThan(0);
-		expect(app.getState().hasList).toBeTrue();
-		expect(app.getState().listing.length).toBeGreaterThan(0);
-	});
-
-	it('should handle when no files are found', async () => {
-		setConfig('photo-frame.folders.photo.folder', 'tests/server/data/does_not_exists');
-		const newState = await generateListing();
-		expect(newState.hasList).toBeTrue();
-		expect(newState.listing).toEqual([]);
-		expect(app.getState().hasList).toBeTrue();
-		expect(app.getState().listing.length).toBe(0);
-	});
-
 	it('should init', async function () {
 		expect(init()).toBeDefined();
 	});
 
+	it('should handle when no files are found', async () => {
+		const data = await loadList('tests/does_not_exists');
+		expect(data.length).toBe(0);
+	});
+
+	it('should get the content', async function () {
+		const data = await loadList('tests/server/data/photos');
+
+		expect(data.length).toBe(7);
+
+		const d0 = data[0];
+		expect(d0.originalFilePath).toBe('photo-frame/f2/m3.jpg');
+		expect(d0.data.title).toBe('');
+		expect(d0.data.date).toBe('');
+		expect(d0.data.orientation).toBe(0);
+	});
 });
