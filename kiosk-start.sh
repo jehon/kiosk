8c7608860f493e5f469dfa4cdb8a5a63909982e1
Jean Honlet <jehon@users.noreply.github.com>
2022-05-13 13:19:24 +0200

    fix: do not npm install since packages are delivered

--- kiosk-start.sh
+++ kiosk-start.sh
@@ -19,32 +19,6 @@ pushd "$KIOSK_ROOT" || exit 255
 	NODE_ENV="${NODE_ENV:-production}"
 	export NODE_ENV
 
-	mkdir -p "tmp"
-	mkdir -p "var"
-	mkdir -p "etc"
-
-	echo "********** Checking setup ************************"
-	PKG=package.json
-	PKG_INST=var/package.json.installed
-	mkdir -p "$(dirname "$PKG_INST")"
-	touch "$PKG_INST"
-
-	# cat | md5sum ? avoid the filename to be shown in the output...
-	if [ "$(md5sum <"$PKG")" == "$(md5sum <"$PKG_INST")" ]; then
-		echo "* Already up-to-date"
-	else
-		echo "* Need an update"
-
-		echo "* ** install **"
-		npm install --unsafe-perm
-
-		echo "* ** prune **"
-		npm prune --unsafe-perm || true
-	fi
-	echo "* ** done **"
-	touch package-lock.json
-	cp -f "$PKG" "$PKG_INST"
-
 	echo "********** Starting session kiosk ************************"
 	npm start -- \
 		--trace-warnings \
