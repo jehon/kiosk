72e59f8151752218f546fb6e26bb4cebacb66a6a
Jean Honlet <jehon@users.noreply.github.com>
2022-05-14 20:29:03 +0200

    fix: no started.log

--- main.cjs
+++ main.cjs
@@ -1,15 +1,6 @@
 
 // Startup log
 
-const fs = require('fs');
-const path = require('path');
-const os = require('os');
-
-fs.writeFileSync(path.join(os.homedir(), 'kiosk-startup-cmd.log'), `
-started
-${JSON.stringify(new Date())}
-
-${JSON.stringify(process.argv, null, 2)}
-`);
+process.stdout.write(`${JSON.stringify(new Date())} - ${JSON.stringify(process.argv, null, 2)}`);
 
 import('./server/server.js');
