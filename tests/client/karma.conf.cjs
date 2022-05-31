b8a46aa850bfa452f19785fcf66bc8e90bc08fc9
Jehon <1582670+jehon@users.noreply.github.com>
2022-05-31 09:52:20 +0000

    test: use chromium

--- tests/client/karma.conf.cjs
+++ tests/client/karma.conf.cjs
@@ -46,6 +46,12 @@ module.exports = function (config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['ChromeHeadless'],
+		browsers: ['ChromiumHeadlessNoSandbox'],
+		customLaunchers: {
+			ChromiumHeadlessNoSandbox: {
+				base: 'ChromiumHeadless',
+				flags: ['--no-sandbox']
+			}
+		},
 	});
 };
