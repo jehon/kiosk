5c07ddd9470a8654c0e3322ceac5bebb21d8a382
Jehon <1582670+jehon@users.noreply.github.com>
2022-06-15 10:49:46 +0000

    fix: mpd launch

--- bin/kiosk-start-mpd-server.sh
+++ bin/kiosk-start-mpd-server.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
+clear
+
+jh-kill-by-port 6600
+
+cat > /etc/default/mpd <<-EOF
+    bind_to_address         "/var/run/mpd.socket"
+    bind_to_address         "localhost"
+EOF
+
 /usr/bin/mpd --no-daemon /etc/default/mpd
