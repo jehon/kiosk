2f8a417c0a378e73c48122ef2f276f683ead432b
Jehon <1582670+jehon@users.noreply.github.com>
2022-07-07 14:07:23 +0000

    refactor: remove websockify

--- packages/music/music-server.mjs
+++ packages/music/music-server.mjs
@@ -1,8 +1,5 @@
 
 import serverAppFactory from '../../server/server-app.js';
-import child_process from 'child_process';
-import nodeCleanup from 'node-cleanup';
-
 import { expressApp, expressAppListener } from '../../server/server-lib-gui-browser.js';
 import { createProxyMiddleware } from '../../node_modules/http-proxy-middleware/dist/index.js';
 
@@ -34,68 +31,11 @@ expressApp.get('/externals/mpd/config.js', (req, res) =>
  * @type {module:server/ServerApp}
  */
 const app = serverAppFactory('music');
-const mpdLogger = app.childLogger('mpd');
-
 export default app;
 
 const status = {
 	config: app.getConfig(),
 };
-
-const MPDServerCommand = [
-	'externals/websockify/run',
-	'8800', // From
-	'localhost:6600' // To
-];
-
-/** @type {child_process.ChildProcess} */
-let socketify = null;
-
-/**
- *
- */
-function startMPD() {
-	if (!socketify) {
-		mpdLogger.debug(`Lauching ${MPDServerCommand.join(' ')}`);
-
-		socketify = child_process.spawn(
-			MPDServerCommand[0],
-			MPDServerCommand.slice(1),
-			{
-				killSignal: 'SIGKILL'
-			},
-			(error, stdout, stderr) => {
-				app.error(`Launching ${MPDServerCommand.join(' ')} gives ${error}: ${stdout} ${stderr}`);
-			}
-		);
-
-		socketify.stdout.setEncoding('utf8');
-		socketify.stdout.on('data', (msg) => {
-			msg.split('\n').forEach(m => mpdLogger.debug(m));
-		});
-
-		socketify.stderr.setEncoding('utf8');
-		socketify.stderr.on('data', (msg) => {
-			msg.split('\n').forEach(m => mpdLogger.error(m));
-		});
-
-		socketify.once('exit', () => {
-			mpdLogger.debug('exited');
-			socketify = null;
-		});
-	}
-}
-
-/**
- *
- */
-function stopMPD() {
-	if (socketify) {
-		mpdLogger.debug('Stopping');
-		mpdLogger.debug('Stopped', socketify.kill('SIGKILL'));
-	}
-	socketify = null;
-}
 
 /**
  * Initialize the package
@@ -103,17 +43,9 @@ function stopMPD() {
  * @returns {module:server/ServerApp} the app
  */
 export function init() {
-	stopMPD();
-	startMPD();
-
 	app.setState(status);
 
 	return app;
 }
-
-nodeCleanup((_exitCode, _signal) => {
-	stopMPD();
-	// return true;
-});
 
 init();
