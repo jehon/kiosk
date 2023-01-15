f8bf8a716da0daf9861b871068356f2ff25b1d6b
Jean Honlet <jehon@users.noreply.github.com>
2023-01-15 17:59:23 +0100

    build: use debian:testing

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o pipefail
+
 export DEBIAN_FRONTEND=noninteractive 
 
-apt update
+find /etc/apt
 
-apt install -y firefox xvfb jehon-service-kiosk
+ls -l /etc/apt/sources.list.d
+
+apt_install() {
+    apt install --yes --quiet "$@"
+}
+
+apt update
+apt_install ca-certificates
+
+apt update
+apt_install jehon
+
+apt update
+apt_install jehon-os-debian
+
+apt update
+apt_install jehon-hardware-docker
+
+apt update
+apt_install firefox-esr xvfb jehon-service-kiosk
