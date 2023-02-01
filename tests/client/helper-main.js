7500ca47796c48567ab0a1e7cbfa6381ec2182d4
Jean Honlet <jehon@users.noreply.github.com>
2023-02-01 15:03:08 +0100

    test: tick for y/m/d too

--- tests/client/helper-main.js
+++ tests/client/helper-main.js
@@ -10,6 +10,9 @@ export function fn(url) {
 /**
  *
  * @param {object} options to wait for
+ * @param {number} options.years to wait for
+ * @param {number} options.months to wait for
+ * @param {number} options.days to wait for
  * @param {number} options.hours to wait for
  * @param {number} options.minutes to wait for
  * @param {number} options.seconds to wait for
@@ -17,13 +20,18 @@ export function fn(url) {
  */
 export function tick(options) {
   options = {
+    years: 0,
+    months: 0,
+    days: 0,
     hours: 0,
     minutes: 0,
     seconds: 0,
     milliSeconds: 0,
     ...options
   };
-  jasmine.clock().tick((((options.hours * 60) + options.minutes) * 60 + options.seconds) * 1000 + options.milliSeconds);
+  jasmine.clock().tick((((
+    ((options.years * 365 + options.months * 30) + options.days * 24)
+    + options.hours * 60) + options.minutes) * 60 + options.seconds) * 1000 + options.milliSeconds);
 }
 
 document.querySelector('body').insertAdjacentHTML('beforeend', '<div id="main-application"></div>');
