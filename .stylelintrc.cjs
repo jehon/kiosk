649678a64dacecfdd962afacc9972367a89ab222
Jean Honlet <jehon@users.noreply.github.com>
2023-02-14 11:03:03 +0100

    Merge renovate/stylelint-15.x

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -4,12 +4,8 @@ module.exports = {
     "node_modules/**/*"
   ],
   "rules": {
-    "indentation": "tab",
     "selector-type-no-unknown": null,
     "no-descending-specificity": null,
-    "declaration-empty-line-before": null,
-    "declaration-colon-space-after": null,
-    "no-empty-first-line": null,
-    "no-missing-end-of-source-newline": null
+    "declaration-empty-line-before": null
   }
 }
