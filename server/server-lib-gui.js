cd010714ffe43848b253aecd0fa976d774954f0e
Jean Honlet <jehon@users.noreply.github.com>
2023-02-15 10:21:54 +0100

    fix: merge gui-browser

--- server/server-lib-gui.js
+++ server/server-lib-gui.js
@@ -1,6 +1,8 @@
-import { guiLaunch, guiPrepare } from "./server-lib-gui-browser.js";
+import Express from "express";
+import getConfig from "./server-lib-config.js";
 
-export { expressApp } from "./server-lib-gui-browser.js";
+export const expressApp = Express();
+export let expressAppListener;
 
 /**
  * @param {module:server/ServerApp} serverApp for logging purpose
@@ -11,7 +13,33 @@ export async function start(serverApp) {
   const url = "client/index.html";
   // const url = `http://localhost:${app.getConfig('.webserver.port')}/client/index.html`;
 
-  await guiPrepare(logger);
+  // to support JSON-encoded bodies
+  expressApp.use(
+    Express.json({
+      strict: false
+    })
+  );
 
-  await guiLaunch(logger, url);
+  getConfig("server.expose", []).forEach((element) => {
+    logger.debug(`Exposing ${element}}`);
+    expressApp.use(element, Express.static(element));
+  });
+  expressApp.use("/client/", Express.static("built"));
+  expressApp.get("/etc/kiosk.yml", (req, res) => res.json(getConfig()));
+  expressApp.use(Express.static("."));
+
+  expressApp.get("/", (req, res) => res.redirect(url));
+  const port = getConfig("core.port", 5454);
+
+  await new Promise((resolve) => {
+    expressAppListener = expressApp.listen(port, function () {
+      // Thanks to https://stackoverflow.com/a/29075664/1954789
+      logger.info(
+        `Listening on port ${this.address().port} (link: 'http://localhost:${
+          this.address().port
+        }')!`
+      );
+      resolve(port);
+    });
+  });
 }
