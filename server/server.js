9e55641ec43e8e3144f580a41e2baff2a7a0e820
Jean Honlet <jehon@users.noreply.github.com>
2023-02-15 15:40:31 +0100

    refactor: one file for server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,37 @@
-import { start } from "./server-lib-gui.js";
-import "../common/command-line-config.js";
+import Express from "express";
+import getConfig from "../common/command-line-config.js";
 
-await start();
+export const expressApp = Express();
+export let expressAppListener;
+
+const url = "client/index.html";
+
+// to support JSON-encoded bodies
+expressApp.use(
+  Express.json({
+    strict: false
+  })
+);
+
+getConfig("server.expose", []).forEach((element) => {
+  expressApp.use(element, Express.static(element));
+});
+
+expressApp.use("/client/", Express.static("built"));
+expressApp.get("/etc/kiosk.yml", (req, res) => res.json(getConfig()));
+expressApp.use(Express.static("."));
+
+expressApp.get("/", (req, res) => res.redirect(url));
+const port = getConfig("core.port", 5454);
+
+await new Promise((resolve) => {
+  expressAppListener = expressApp.listen(port, function () {
+    // Thanks to https://stackoverflow.com/a/29075664/1954789
+    console.info(
+      `Listening on port ${this.address().port} (link: 'http://localhost:${
+        this.address().port
+      }')!`
+    );
+    resolve(port);
+  });
+});
