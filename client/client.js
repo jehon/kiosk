f69af43bf5583d3b484246adfc14a61ad075f983
Jean Honlet <jehon@users.noreply.github.com>
2023-02-16 14:43:05 +0100

    chore: remove error handler

--- client/client.js
+++ client/client.js
@@ -1,19 +1,5 @@
-import { ClientApp, waitForConfig } from "./client-app.js";
+import { waitForConfig } from "./client-app.js";
 import { autoSelectApplication } from "./client-lib-chooser.js";
-
-/*
- * Catch all errors and send them to the backend
- */
-const globalCatcher = new ClientApp("catch");
-window.addEventListener("error", (event) => {
-  globalCatcher.error(
-    event.message,
-    event.filename
-      ? event.filename + "#" + event.lineno + ":" + event.colno
-      : "",
-    event.error
-  );
-});
 
 //
 // Load other packages
